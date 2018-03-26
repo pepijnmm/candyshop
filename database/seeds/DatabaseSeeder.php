5d0bb80bf73fa76f0828de16d2d4d7f9dc6342d8
Arcaev <Jorrit-93@hotmail.com>
2018-03-26 12:04:17 +0200

    Added admin + user login (password admin>admin, user>user)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Hashing\BcryptHasher;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +18,26 @@ class DatabaseSeeder extends Seeder
         factory(App\Order::class, 10)->create();
         factory(App\Category::class, 10)->create();
         factory(App\Address::class, 10)->create();
+
+        DB::table('users')->insert(
+            [
+                'first_name' => 'user',
+                'second_name' => 'user',
+                'email' => 'user',
+                'role'=> 0,
+                'password' => '$2y$10$mM/SFzzfgLGEKhoHijJTTevTmXMVX0scbebucdr4XubqGH9HACSZy', // user
+            ]
+        );
+
+        DB::table('users')->insert(
+            [
+                'first_name' => 'admin',
+                'second_name' => 'admin',
+                'email' => 'admin',
+                'role'=> 1,
+                'password' =>'$2y$10$.2MTtwETa71tphE2NLEn3.FHzB2pSPwjauNBWIGpixvBWeqJ5CIWu', // admin
+            ]
+        );
 
         for ($x = 1; $x <= random_int(1, 10); $x++) {
             for ($y = 1; $y <= random_int($x + 1, 10); $y++) {
