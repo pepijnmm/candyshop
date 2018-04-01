8ae541b3dcaba06f66b578e261677b1853f1defc
Arcaev <Jorrit-93@hotmail.com>
2018-04-01 17:35:36 +0200

    fixed homepage

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,13 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        'price' => random_int(001, 999) / 100,
+        'price' => random_int(3, 20) * 0.50,
         'name' => $faker->name,
         'small_description' => substr($faker->paragraph,0,200),
         'description' => $faker->paragraph,
         'weight' => random_int(100, 999) / 100,
         'storage' => random_int(10000, 99999),
-        'discount' => random_int(10, 99),
+        'discount' => random_int(0, 2)*20,
         'image_location' => 'chocola.jpg',
     ];
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,42 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="nine columns" id="oneproduct">
-    
-	<a href="{{ (!empty($products[0]))?action('ProductController@show',$products[0]->id):''   }}" class="row">
-        <div class="six columns">
-			<h2 id="producttitle">{{ (!empty($products[0]))?$products[0]->name:'' }}</h2>
-			    <hr>
-            <img src="images/{{ (!empty($products[0]))?$products[0]->image_location:''}}" alt="product plaatje"/>
-            <hr>
-            <p id="producttitle">{{ (!empty($products[0]))?$products[0]->small_description:'' }}</p>
+    <div style="text-align: center; display: flex; flex-direction: column">
+        <h2 style="align-self: center; ">Welkom bij Ye Sweet Shoppe!</h2>
+        <img style="width: 500px; align-self: center; margin: 10px" src="images/{{ (!empty($products[0]))?$products[0]->image_location:''}}" alt="product plaatje"/>
+        <div style="align-self: center; display: inline-flex; flex-direction: row; flex-wrap: nowrap; justify-content: center">
+            <table style="padding: 10px; width: 400px">
+                <caption>Ons assortiment:</caption>
+                <tr>
+                    <th></th>
+                    <th>Naam</th>
+                    <th>Prijs</th>
+                </tr>
+                @foreach ($products as $product)
+                    <tr>
+                        <td><img src="/images/{{$product->image_location}}" class="categoryproductpicture" alt="product plaatje"/></td>
+                        <td><a href="{{ action('ProductController@show', $product) }}">{{$product->name}}</a></td>
+                        <td><a href="{{ action('ProductController@show', $product) }}">€{{$product->price}}</a></td>
+                    </tr>
+                @endforeach
+            </table>
+            <table style="padding: 10px; width: 400px">
+                <caption>Onze aanbiedingen:</caption>
+                <tr>
+                    <th></th>
+                    <th>Naam</th>
+                    <th>Prijs</th>
+                    <th>Korting</th>
+                </tr>
+                @foreach ($products->where('discount', '<>', null) as $product)
+                    <tr>
+                        <td><img src="/images/{{$product->image_location}}" class="categoryproductpicture" alt="product plaatje"/></td>
+                        <td><a href="{{ action('ProductController@show', $product) }}">{{$product->name}}</a></td>
+                        <td><a href="{{ action('ProductController@show', $product) }}">€{{$product->price}}</a></td>
+                        <td><a href="{{ action('ProductController@show', $product) }}">{{$product->discount}}%</a></td>
+                    </tr>
+                @endforeach
+            </table>
         </div>
-	</a>
-</div>
+    </div>
 @endsection
